cef5b6176653ea398b447d9fdbda19baa155be7e
Nalin Adhikari <csitplay@gmail.com>
2016-09-13 12:00:18 +0545

    index page modified with addition of bootstrap and landing page modified

--- App.jsx
+++ App.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-         <div>
-            Hello World!!!
+         <div className = "row marginTop120">
+         	<div className = "col-md-4"></div>
+         	<div className = "col-md-4 textCenter">
+         		<h2 className = "alert alert-info" >Hello World</h2>
+         		<h4 >from</h4>
+         		<h3 className = "alert alert-success" >A React Todos App</h3>
+         	</div>
+         	<div className = "col-md-4"></div>
          </div>
       );
    }
