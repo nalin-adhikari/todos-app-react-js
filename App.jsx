0917e385221cee4abef39fc602d7a0813600469c
Nalin Adhikari <csitplay@gmail.com>
2016-09-16 15:20:08 +0545

    Greeting class created and called from App class.

--- App.jsx
+++ App.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-         <div className = "row marginTop120">
-         	<div className = "col-md-4"></div>
-         	<div className = "col-md-4 textCenter">
-         		<h2 className = "alert alert-info" >Hello World</h2>
-         		<h4 >from</h4>
-         		<h3 className = "alert alert-success" >A React Todos App</h3>
-         	</div>
-         	<div className = "col-md-4"></div>
-         </div>
+         <Greeting />
       );
+   }
+}
+
+class Greeting extends React.Component{
+   render(){
+      return (
+            <div className = "row greeting">
+               <div className = "col-md-4"></div>
+               <div className = "col-md-4 textCenter">
+                  <h3 className = "alert alert-success" >A React Todos App</h3>
+               </div>
+               <div className = "col-md-4"></div>
+            </div>
+         );
    }
 }
 
