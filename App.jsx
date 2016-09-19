622ae8dfb47546097c52b21bf7324a77dde9c4a8
abrial <abiral@fusemachines.com>
2016-09-19 14:32:44 +0545

    parse int index added

--- App.jsx
+++ App.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 class App extends React.Component {
 
-  getInitialState(){
-    var todos = [];
-    var completed = [];
-    if(localStorage.todos && localStorage.completed){
-      todos = JSON.parse(localStorage.todos);
-      completed = JSON.parse(localStorage.completed);
-    }else{
-      //Insert dummy todos
-      todos.push("todos one");
-      completed.push("true");
-    }
+  // getInitialState(){
+  //   var todos = [];
+  //   var completed = [];
+  //   if(localStorage.todos && localStorage.completed){
+  //     todos = JSON.parse(localStorage.todos);
+  //     completed = JSON.parse(localStorage.completed);
+  //   }else{
+  //     //Insert dummy todos
+  //     todos.push("todos one");
+  //     completed.push("true");
+  //   }
 
-    return{
-      todos: todos,
-      completed: completed
-    };
-  }
+  //   return{
+  //     todos: todos,
+  //     completed: completed
+  //   };
+  // }
 
    render() {
       return (
@@ -49,9 +49,12 @@ class Greeting extends React.Component{
 
 class TodoForm extends React.Component{
 
-   addTodos(){
-    var todo = document.getElementById("todoItem");
-   }
+  addTodos(){
+    var todo = document.getElementById('todoItem').value;
+    //console.log(todo);
+    todos.push(todo);
+    console.log(todos);    
+  }
 
    render(){
       return (
@@ -59,29 +62,46 @@ class TodoForm extends React.Component{
             <div className="col-lg-10">
               <input type="text" className="form-control" id="todoItem" placeholder="Todo task ..." />
             </div>
-            <button type="button" className="btn btn-primary" onClick={this.addTodos()} >Create</button>
+            <button type="button" className="btn btn-primary" onClick={this.addTodos} >Insert</button>
           </div>
          );
    }
 }
 
 class TodoList extends React.Component{
-   render(){
+ 
+  render(){
       return (
             <div className="form-group">
-               <div className="col-lg-10">
-                 <div className="checkbox">
-                 <button type="button" className="close" data-dismiss="alert">&times;</button>
-                   <label>
-                     <input type="checkbox" /> {this.todos}
-                   </label>
-                 </div>
+               <div className="col-lg-10" id="list">
+                {todos.map(function(item, index){
+                    return <TodoListItem key={index} item={item} index={index} />;
+                })}
                </div>
              </div>
          );
    }
 }
 
-var todos = localStorage.getItem('todos') || [];
+class TodoListItem extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="checkbox">
+        <button type="button" className="close" data-dismiss="alert">&times;</button>
+        <label>
+          <input type="checkbox" /> { todos[parseInt(this.props.index)] }
+        </label>
+      </div>
+    );
+  }
+}
+
+// var todos = localStorage.getItem('todos') || [];
+var todos = ["hi", "hello"];
+
 
 export default App;
